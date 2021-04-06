9acb8f2c8d469650a2afa3b930d1564941dc369e
Shreyas Babu <shreyas.babu28@gmail.com>
2021-04-06 08:30:42 -0700

    displaying weather data

---
+++ WeatherApp/WeatherApp/WeatherCell/WeatherCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Shreyas Babu on 4/6/21.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "WeatherCollectionViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "WeatherCollectionViewCell",
+                     bundle: nil)
+    }
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+
+    func configure(with model: HourlyWeatherEntry) {
+        self.tempLabel.text = "\((Int(model.temperature) - 32) * 5/9)°"
+        self.iconImageView.contentMode = .scaleAspectFit
+        self.iconImageView.image = UIImage(named: "clear")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
